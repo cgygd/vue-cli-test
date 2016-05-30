9dc4d5ad5c86b892f6fcf61e61d6d88bbf1463aa
小鱼 <80447081@qq.com>
2016-05-30 23:59:59 +0800

    增加弹出层组件
    增加弹出层组件

--- src/views/order/Orderdetails.vue
+++ src/views/order/Orderdetails.vue
@@ -1,14 +1,36 @@
+
+
 <template>
-    <div class="main marginTop100">
-        <h2>这是订单详情{{$route.params.orderid}}，三级路由</h2>
-        <Display></Display>
-    </div>
+
+<div class="main marginTop100">
+    <h2>这是订单详情{{$route.params.orderid}}，三级路由</h2>
+    <Display></Display>
+
+
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <modal :show.sync="showModal">
+        <h3 slot="header">custom header</h3>
+    </modal>
+
+
+</div>
+
 </template>
+
 <script type="text/javascript">
-    import Display from '../../components/Display'
-    export default{
-        components:{
-            Display,
+
+import Display from '../../components/Display'
+import Modal from './Modal'
+export default {
+    components: {
+        Display,
+        Modal
+    },
+    data() {
+        return {
+            showModal: false
         }
     }
+}
+
 </script>
